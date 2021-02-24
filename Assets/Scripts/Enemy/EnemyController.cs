f22427a0652070f8dd2baeb393d1008a6ed79dd5
Pedro Esteves <up201705160@fe.up.pt>
2021-02-24 21:24:03 +0000

    Added Health script and IHealthController interface

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyController : MonoBehaviour
+public class EnemyController : MonoBehaviour, IHealthController
 {
     [SerializeField]
     private float lookRadius = 10f;
