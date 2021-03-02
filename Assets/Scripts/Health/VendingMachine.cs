c876a95454274f59501ff66c91284c15ef0582bd
Nuno Cardoso <up201706162@fe.up.pt>
2021-03-02 15:38:10 +0000

    Vending machine only works once

--- Assets/Scripts/Health/VendingMachine.cs
+++ Assets/Scripts/Health/VendingMachine.cs
@@ -6,17 +6,19 @@ public class VendingMachine : MonoBehaviour
     private GameObject key = null;
     private int range = 5;
     private float healFactor = 50f;
+    private bool active = true;
 
     // Update is called once per frame
     void Update()
     {
-        if ((Player.GetInstance().transform.position - this.transform.position).magnitude < range)
+        if ((Player.GetInstance().transform.position - this.transform.position).magnitude < range && active)
         {
             key.SetActive(true);
             if (Input.GetButtonDown("Heal"))
             {
                 Health health = Player.GetInstance().GetComponent<Health>();
                 health.Heal(healFactor);
+                active = false;
             }
         }
         else key.SetActive(false);
