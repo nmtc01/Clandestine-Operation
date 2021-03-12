52dbe1558fdf9b8c2cee9ee550fb80032483c044
Pedro Esteves <up201705160@fe.up.pt>
2021-03-12 22:28:47 +0000

    Added heal sound. Moved some sounds to the resources folder

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -4,10 +4,16 @@ public class PlayerHealth : Health
 {
     private bool playedDeathSound = false;
 
+    [SerializeField]
+    private string deathClipPath = "", healClipPath = "";
+    private AudioSource audioSource;
+
     protected override void Start()
     {
         healthUIController = PlayerHealthUI.instance;
         currentHealth = healthUIController.GetValue();
+
+        audioSource = GetComponent<AudioSource>();
 
         base.Start();
     }
@@ -21,7 +27,24 @@ public class PlayerHealth : Health
         if (!playedDeathSound)
         {
             playedDeathSound = true;
-            GetComponent<AudioSource>().Play();        
+
+            AudioClip deathClip = Resources.Load<AudioClip>(deathClipPath);
+            PlayClip(deathClip);
         }
+    }
+
+    public override void Heal(float value)
+    {
+        base.Heal(value);
+
+        AudioClip healClip = Resources.Load<AudioClip>(healClipPath);
+        PlayClip(healClip);
+    }
+
+    private void PlayClip(AudioClip ac)
+    {
+        audioSource.clip = ac;
+        audioSource.Play();
+
     }
 }
