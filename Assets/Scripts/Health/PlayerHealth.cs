0b7b4709fd503e2e8243764be6b82277a35d80b4
Pedro Esteves <up201705160@fe.up.pt>
2021-03-13 15:43:42 +0000

    Added hearth beat sound. Decreased the timer alarm volume.

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -6,16 +6,20 @@ public class PlayerHealth : Health
 
     [SerializeField]
     private string deathClipPath = "", healClipPath = "";
-    private AudioSource audioSource;
+    
+    [SerializeField]
+    private AudioSource mainAudioSource = null, hearthBeatAudioSource = null;
+
+    private static float minHearthBeatVolume = .05f;
 
     protected override void Start()
     {
         healthUIController = PlayerHealthUI.instance;
-        currentHealth = healthUIController.GetValue();
-
-        audioSource = GetComponent<AudioSource>();
+        currentHealth = healthUIController.GetValue(); 
 
         base.Start();
+
+        UpdateHearthBeatAudioSource();
     }
 
     public override void Kill()
@@ -30,6 +34,8 @@ public class PlayerHealth : Health
 
             AudioClip deathClip = Resources.Load<AudioClip>(deathClipPath);
             PlayClip(deathClip);
+
+            hearthBeatAudioSource.Stop();
         }
     }
 
@@ -39,12 +45,26 @@ public class PlayerHealth : Health
 
         AudioClip healClip = Resources.Load<AudioClip>(healClipPath);
         PlayClip(healClip);
+
+        UpdateHearthBeatAudioSource();
+    }
+
+    public override void Damage(float damage)
+    {
+        base.Damage(damage);
+
+        UpdateHearthBeatAudioSource();
+    }
+
+    private void UpdateHearthBeatAudioSource()
+    {
+        hearthBeatAudioSource.volume = minHearthBeatVolume + (maxHealth - currentHealth) / maxHealth;
+        hearthBeatAudioSource.pitch = 1 + (hearthBeatAudioSource.volume - minHearthBeatVolume) / 2f;
     }
 
     private void PlayClip(AudioClip ac)
     {
-        audioSource.clip = ac;
-        audioSource.Play();
-
+        mainAudioSource.clip = ac;
+        mainAudioSource.Play();
     }
 }
